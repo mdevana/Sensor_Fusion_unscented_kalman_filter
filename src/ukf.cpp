d77317e682a5f9bbf85fde40a2c437e0c153e07d
mdevana <madhusudhan.d@gmail.com>
2021-11-05 12:55:23 +0100

    first commit

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -114,6 +114,7 @@ void UKF::UKF_test_initialise(){
 }
 
 void UKF::PrintData(){
+	std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
 	std::cout << "Xsig_pred = " << std::endl << Xsig_pred_ << std::endl;
 	std::cout << "Weights = " << std::endl << weights_<< std::endl;
 	std::cout << "x_ = " << std::endl << x_<< std::endl;
