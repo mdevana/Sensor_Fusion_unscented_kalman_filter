32ae1006183ffc62f48ee806465c2cd8417301a3
mdevana <madhusudhan.d@gmail.com>
2021-11-06 06:18:54 +0100

    integrate

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,7 @@ int main() {
 		  
           estimations.push_back(estimate);
 		  VectorXd RMSE(4);
-          //VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
+          VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
 		  
 		  std::cout << "RMSE values :"<< RMSE(0)<<" "<<RMSE(1)<<" "<<RMSE(2)<<" "<<RMSE(3)<<std::endl;
 
