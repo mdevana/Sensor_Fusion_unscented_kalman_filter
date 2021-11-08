b030d4afcf238ec565c13e40454cb2fdac826f66
mdevana <madhusudhan.d@gmail.com>
2021-11-08 06:24:35 +0100

    integrate

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -357,14 +357,14 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_pack) {
    init_test();
    dt=0.1;
    std::cout << "Time Step reinitialised: " <<dt<<std::endl;
-   while (dt> 0.1 ) {
+   /*while (dt> 0.1 ) {
    	   std::cout << "into while loop" <<dt<<std::endl;
    AugmentSigmaPoint();
    PredictSigmaPoint(0.05);
    PredictMeanCovariance();
    dt-=0.05;
 	   
-   }
+   }*/
    AugmentSigmaPoint();
    PredictSigmaPoint(dt);
    PredictMeanCovariance();
